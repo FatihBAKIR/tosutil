96e6d23b5e4a54a551668628e047cd73f92203b0
Fatih BAKIR <fatih@linux.com>
2018-12-15 01:37:37 -0800

    atmega328p processor

--- main.cpp
+++ main.cpp
@@ -4,7 +4,11 @@
 
 int main() {
     std::cout
-    << tut::load_proc("STM32F103RB")->name << '\n'
-    << tut::load_proc("STM32F103RB")->target->arch->name << '\n';
+            << tut::load_proc("STM32F103RB")->name << '\n'
+            << tut::load_proc("STM32F103RB")->target->arch->name << '\n';
+    std::cout << '\n';
+    std::cout
+            << tut::load_proc("atmega328p")->name << '\n'
+            << tut::load_proc("atmega328p")->target->arch->name << '\n';
     return 0;
 }
