b49bdbd3c60171c8c0f7792a104f1b12972c3bfb
Fatih Bakir <mfatihbakir@gmail.com>
2018-12-16 12:09:23 -0800

    up

--- src/src/apps/program.cpp
+++ src/src/apps/program.cpp
@@ -8,7 +8,18 @@
 namespace bp = boost::process;
 namespace fs = boost::filesystem;
 
-void program_openocd(const tut::programmer_args_t& pargs, const std::string& program)
+struct program_descriptor
+{
+    fs::path full_path;
+    tut::elf_ptr elf;
+};
+
+void program_avrdude(const tut::programmer_args_t& pargs, const program_descriptor& program)
+{
+    // must run objcopy etc if elf, needs binutils
+}
+
+void program_openocd(const tut::programmer_args_t& pargs, const program_descriptor& program)
 {
     fs::path openocd_root = "/usr";
 	auto openocd_executable = openocd_root / "bin/openocd";
@@ -17,11 +28,11 @@ void program_openocd(const tut::programmer_args_t& pargs, const std::string& pro
 
 	std::vector<std::string> args;
 	args.push_back("-s");
-	args.push_back(fmt::format("{}", script_search_path.string()));
+	args.push_back(script_search_path.string());
 	args.push_back("-f");
-	args.push_back(fmt::format("{}", pargs.args["file"]));
+	args.push_back(pargs.args["file"]);
 	args.push_back("-c");
-	args.push_back(fmt::format("program {} verify reset exit", program));
+	args.push_back(fmt::format("program {} verify reset exit", program.full_path.string()));
 
 	std::cout << "+ " << openocd_executable << " ";
 	for (auto& arg : args)
@@ -35,8 +46,26 @@ void program_openocd(const tut::programmer_args_t& pargs, const std::string& pro
 
 int main(int argc, char** argv)
 {
-	auto full_path = fs::canonical(argv[2]);
-	auto board = tut::load_board(argv[1]);
+    program_descriptor desc;
 
-	program_openocd(board->programmers[0], full_path.string());
+    desc.full_path = fs::canonical(argv[2]);
+    desc.elf = tut::load_elf(desc.full_path.string());
+
+	auto board = tut::load_board(argv[1]);
+	auto& programmer = board->programmers[0];
+
+	std::map<std::string, void(*)(const tut::programmer_args_t&, const program_descriptor&)>
+	        programmers;
+	programmers.emplace("openocd", &program_openocd);
+	programmers.emplace("avrdude", &program_avrdude);
+
+	auto programmer_it = programmers.find(programmer.programmer->alias);
+
+	if (programmer_it == programmers.end())
+    {
+	    std::cerr << "programmer " << programmer.programmer->alias <<  " not found";
+	    return 1;
+    }
+
+	programmer_it->second(board->programmers[0], desc);
 }
