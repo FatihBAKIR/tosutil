c7b86b1cd545940918c0b2170afc858448a3fd23
oguzbakir <b21627007@cs.hacettepe.edu.tr>
2019-01-06 17:18:02 +0300

    Cleanup

--- src/src/apps/prepare/prepare.cpp
+++ src/src/apps/prepare/prepare.cpp
@@ -12,6 +12,8 @@
 #include <boost/process.hpp>
 #include <boost/filesystem/path.hpp>
 #include <boost/system/error_code.hpp>
+#include <cpprest/http_client.h>
+
 
 #include "prepare.hpp"
 
@@ -21,14 +23,6 @@
 #define SHELL_COMMAND_PREFIX "/c"
 #endif
 
-// Check windows
-#if _WIN32 || _WIN64
-#if _WIN64
-#define ENV64BIT
-#else
-#define ENV32BIT
-#endif
-#endif
 
 // Check GCC
 #if __GNUC__
@@ -46,14 +40,13 @@
 #define x86_64 1
 #endif
 
-#if defined(BOOST_POSIX_API)
-#define LINUX 1
-#elif defined(BOOST_WINDOWS_API)
-#define LINUX 0
-#endif
+
 
 namespace bp = boost::process;
 namespace fs = boost::filesystem;
+
+using namespace web::http;
+using namespace web::http::client;
 
 std::string getDistro() {
     std::string ret;
@@ -76,67 +69,65 @@ std::string getDistro() {
 
 void getFile(const std::string &link, const std::string &dest, const std::string &name) {
 
+    fs::path wget_root = "/usr";
+    std::vector<std::string> args;
 
-    if (LINUX) {
-        fs::path wget_root = "/usr";
-        std::vector<std::string> args;
+    auto wget_executable = bp::search_path("wget", {wget_root / "bin"});
 
-        auto wget_executable = bp::search_path("wget", {wget_root / "bin"});
-
-        if (wget_executable.empty()) {
-            throw std::runtime_error("Could not find wget executable");
-        }
-
-        args.emplace_back("-nc"); // If file exists do not download
-
-        args.emplace_back(link);
-        args.emplace_back("-O");
-        args.emplace_back(dest + name);
-
-
-        bp::ipstream pipe_stream;
-        bp::child c(wget_executable, args, bp::std_out > pipe_stream);
-
-        std::string line;
-        while (pipe_stream && std::getline(pipe_stream, line) && !line.empty())
-            std::cout << line << std::endl;
-
-        c.wait();
-
-    } else { // Windows
-
+    if (wget_executable.empty()) {
+        throw std::runtime_error("Could not find wget executable");
     }
+
+    args.emplace_back("-nc"); // If file exists do not download
+
+    args.emplace_back(link);
+    args.emplace_back("-O");
+    args.emplace_back(dest + name);
+
+
+    bp::ipstream pipe_stream;
+    bp::child c(wget_executable, args, bp::std_out > pipe_stream);
+
+    std::string line;
+    while (pipe_stream && std::getline(pipe_stream, line) && !line.empty())
+        std::cout << line << std::endl;
+
+    c.wait();
 
 
 }
 
 
-void extract(const std::string &filename) {
+/*
+ * c:/xutils
+ * /opt/xutils
+ *
+ *
+ */
+
+void extract(const std::string &filename, const std::string &dest) { // nereye acilacagini al
     std::string exe;
     std::vector<std::string> args;
 
-    if (LINUX) {
-        fs::path l7z_root = "/usr";
-        auto l7z_executable = bp::search_path("7z", {l7z_root / "bin"});
+    fs::path l7z_root = "/usr";
+    auto l7z_executable = bp::search_path("7z", {l7z_root / "bin"});
 
-        if (l7z_executable.empty()) {
-            throw std::runtime_error("Could not find l7z executable");
-        }
-        args.emplace_back("x"); // For extract
-        args.emplace_back("-y"); // assume Yes on all queries. eg: override files
-        args.emplace_back(filename);
-        bp::ipstream pipe_stream;
-        bp::child c(l7z_executable, args, bp::std_out > pipe_stream);
-
-        std::string line;
-        while (pipe_stream && std::getline(pipe_stream, line) && !line.empty())
-            std::cout << line << std::endl;
-
-        c.wait();
-
-    } else { // Windows
-
+    if (l7z_executable.empty()) {
+        throw std::runtime_error("Could not find l7z executable");
     }
+    args.emplace_back("x"); // For extract
+    args.emplace_back("-y"); // assume Yes on all queries. eg: override files
+    args.emplace_back(filename);
+    args.emplace_back("-o " + dest);
+    bp::ipstream pipe_stream;
+    bp::child c(l7z_executable, args, bp::std_out > pipe_stream);
+
+    std::string line;
+    while (pipe_stream && std::getline(pipe_stream, line) && !line.empty())
+        std::cout << line << std::endl;
+
+    c.wait();
+
 
 }
 
@@ -153,64 +144,74 @@ int main(int argc, char **argv) try {
     }
 
 
-    if (!strcmp(argv[1], "board")) {
-        if (LINUX) {
-            if (getDistro() == "Ubuntu") {
-                if (!strcmp(argv[2], "nucleo/f103rb")) {
-                    fs::path openocd_root = "/usr";
-                    auto openocd_executable = bp::search_path("openocd", {openocd_root / "bin"});
+    std::string arg1(argv[1]);
+    std::string arg2(argv[2]);
+    if (arg1 == "board") {
+        if (getDistro() == "Ubuntu") {
+            if (arg2 == "nucleo/f103rb") {
+                fs::path openocd_root = "/usr";
+                auto openocd_executable = bp::search_path("openocd", {openocd_root / "bin"});
 
-                    if (!openocd_executable.empty()) {
-                        //TODO: request new version if avaiable
-                        throw std::runtime_error("openocd already installed!");
-                    }
-
-                    // Install openocd on ubuntu
-                    fs::path root = "/usr";
-                    std::vector<std::string> args;
-
-                    auto apt_executable = bp::search_path("apt-get", {root / "bin"});
-
-                    args.emplace_back("install");
-                    args.emplace_back("-y");
-                    args.emplace_back("openocd");
-
-                bp::ipstream pipe_stream;
-                bp::child c(apt_executable, args, bp::std_out > pipe_stream);
-
-                std::string line;
-                while (pipe_stream && std::getline(pipe_stream, line) && !line.empty())
-                    std::cout << line << std::endl;
-
-                c.wait();
-
-                    // End of Install openocd on ubuntu
-
-                    // Install avr-gcc
-
-
-
-                    //std::string openocdLink = "https://github.com/ntfreak/openocd/archive/v0.10.0.tar.gz";
-                    //getFile(openocdLink, "./openocd/", "openocd-v0.10.0.tar.gz");
-
+                if (!openocd_executable.empty()) {
+                    //TODO: request new version if avaiable
+                    throw std::runtime_error("openocd already installed!");
                 }
-            } else if (getDistro() == "Fedora") {
-                // Install openocd on fedora
-                // End of Install openocd on fedora
-            } else {
-                std::cout << "Unsupported distro" << std::endl;
+
+                // Install openocd on ubuntu
+//                    fs::path root = "/usr";
+//                    std::vector<std::string> args;
+//
+//                    auto apt_executable = bp::search_path("apt-get", {root / "bin"});
+//
+//                    args.emplace_back("install");
+//                    args.emplace_back("-y");
+//                    args.emplace_back("openocd");
+//
+//                bp::ipstream pipe_stream;
+//                bp::child c(apt_executable, args, bp::std_out > pipe_stream);
+//
+//                std::string line;
+//                while (pipe_stream && std::getline(pipe_stream, line) && !line.empty())
+//                    std::cout << line << std::endl;
+//
+//                c.wait();
+
+                // End of Install openocd on ubuntu
+
+
+
+                // Install avr-gcc on ubuntu
+                http_client client(U("https://api.bakir.io/"));
+                uri_builder builder(U("get-url/x86-linux/avr-gcc"));
+
+                http_response response = client.request(methods::GET, builder.to_string()).get();
+
+                if (response.status_code() == status_codes::OK) {
+                    auto body = response.extract_string();
+                    std::cout << body.get().c_str() << std::endl;
+                }
+
+
+
+                //std::string openocdLink = "https://github.com/ntfreak/openocd/archive/v0.10.0.tar.gz";
+                //getFile(openocdLink, "./openocd/", "openocd-v0.10.0.tar.gz");
+
             }
+        } else if (getDistro() == "Fedora") {
+            // Install openocd on fedora
+            // End of Install openocd on fedora
         } else {
-            // TODO: Windows :D
+            std::cout << "Unsupported distro" << std::endl;
         }
 
-    } else if (!strcmp(argv[1], "proc")) {
-        if (!strcmp(argv[2], "esp8266")) {
-            std::cout << argv[2] << std::endl;
-        } else if (!strcmp(argv[2], "STM32/F103RB")) {
-            std::cout << argv[2] << std::endl;
-        } else if (!strcmp(argv[2], "cortex-m3")) {
-            std::cout << argv[2] << std::endl;
+
+    } else if (arg1 == "proc") {
+        if (arg2 == "esp8266") {
+            std::cout << arg2 << std::endl;
+        } else if (arg2 == "STM32/F103RB") {
+            std::cout << arg2 << std::endl;
+        } else if (arg2 == "cortex-m3") {
+            std::cout << arg2 << std::endl;
         }
     } else {
         std::cerr << "Please use board or proc" << std::endl;
